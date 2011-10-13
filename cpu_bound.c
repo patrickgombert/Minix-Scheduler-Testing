d2860d72010c4d24d2483373f714e820b1a25c23
patrickgombert <patrickgombert@gmail.com>
2011-10-12 23:05:36 -0500

    CPU bound and partially done with IO bound

---
+++ cpu_bound.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+
+int is_palindrome(int subject) {
+  char str[6];
+  sprintf(str, "%d", subject);
+  int len = strlen(str);
+  int i = 0;
+  while(i < len/2) {
+    if (str[i] != str[len - i - 1]){
+      return 1;
+    }
+    i++;
+  }
+  return 0;
+}
+
+int main(int arg, char **argv) {
+  int super_i = 0;
+  while(super_i < 125) {
+    int i = 100;
+    int j = 100;
+    int largest_i = 0;
+    int largest_j = 0;
+    int largest = 0;
+    while(i <= 999) {
+      while(j <= 999) {
+        j++;
+        int tmp = i * j;
+        if(is_palindrome(tmp) == 0 && tmp > largest) {
+          largest_i = i;
+          largest_j = j;
+          largest = tmp;
+        }
+      }
+      i++;
+      j = 100;
+    }
+    super_i++;
+  }
+}
---
+++ io_bound.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int arg, char **argv) {
+  int i = 0;
+  int j = 0;
+  while(i < 32000) {
+    while(j < 32000) {
+      char* tmp1[9999];
+      char* tmp2[9999];
+      FILE *fp;
+      fp=fopen("test1.txt", "r");
+      fread(tmp1, sizeof(tmp1), sizeof(tmp1)/sizeof(char), fp);
+      fclose(fp);
+      fp=fopen("test2.txt", "r");
+      fread(tmp2, sizeof(tmp2), sizeof(tmp2)/sizeof(char), fp);
+      fclose(fp);
+      fp=fopen("test2.txt", "w");
+      fwrite(tmp1, sizeof(tmp1), sizeof(tmp1)/sizeof(char), fp);
+      fclose(fp);
+      fp=fopen("test1.txt", "w");
+      fwrite(tmp2, sizeof(tmp2), sizeof(tmp2)/sizeof(char), fp);
+      fclose(fp);
+      j++;
+    }
+    i++;
+  }
+}
